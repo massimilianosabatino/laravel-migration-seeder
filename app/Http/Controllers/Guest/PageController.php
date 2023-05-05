4671d9668f8eb438155399a0f8bec2de9ceec083
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-05 19:12:49 +0200

    change: get trains from today

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
 
-        $trains = Train::all();
+        // $trains = Train::all();
+        $trains = Train::where('departure_date', '>=', Carbon::today() )->orderBy('departure_date')->get();
 
         return view('index', compact('trains'));
     }
